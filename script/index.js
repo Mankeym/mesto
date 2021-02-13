4bc2ac73e11e2a346066170e73c7422f78e28061
Mankeym <65279886+Mankeym@users.noreply.github.com>
2021-02-13 12:59:23 +0500

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -4,10 +4,47 @@ let nameInput = document.querySelector('.popup__input_type_name')
 let jobInput = document.querySelector('.popup__input_type_job')
 let name = document.querySelector('.profile__title')
 let job = document.querySelector('.profile__subtitle')
+let NameMesto = document.querySelector('.card__title')
+let LinkMesto = document.querySelector('.card__logo')
 let closeOverlay = document.querySelector('.overlay__button')
 let formElement = document.querySelector('.popup__form')
+let form = document.querySelector('.picture__popup__form')
 let picture = document.querySelector('.picture')
 let OpenPicture = document.querySelector('.profile__button')
+let closePicture = document.querySelector('.picture__popup__button')
+let CardContent = document.querySelector('.card-template');
+let namePicture = document.querySelector('.picture__popup__input_type_name')
+let jobPicture = document.querySelector('.picture__popup__input_type_job')
+const directorsList = document.querySelector('.cards');
+let templateEl = document.querySelector('.card-template');
+let LikeBlack = document.querySelector('.card__button');
+let img = document.querySelector('.card__picture')
+const initialCards = [
+    {
+      Mesto: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      Mesto: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      Mesto: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      Mesto: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      Mesto: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      Mesto: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
 let togglePopupOpen = () => {
     overlay.classList.toggle('overlay_active')
     if(overlay.classList.contains('overlay_active')){
@@ -44,8 +81,59 @@ let togglePictureOpen = () => {
     picture.classList.toggle('picture_active')
 }
 OpenPicture.addEventListener('click', togglePictureOpen)
-picture.addEventListener('mouseup', (event) => {
+closePicture.addEventListener('click',togglePictureOpen)
+overlay.addEventListener('mouseup', (event) => {
     if (event.target === event.currentTarget) {
         togglePictureOpen()
     }
 })
+function render() {
+  const html = initialCards
+      .map(getItem)
+
+  directorsList.append(...html);
+}
+function getItem(item) {
+  const newItem = CardContent.cloneNode(true).content
+  const headerEl = newItem.querySelector('.card__title');
+  headerEl.textContent = item.Mesto;
+  const head = newItem.querySelector('.card__logo')
+  head.src = item.link
+  const img = newItem.querySelector('.card__picture')
+  const bigImage = newItem.querySelector('.card__bigimage')
+  bigImage.src = item.link
+  const textpicture = newItem.querySelector('.card__textpicture')
+  textpicture.textContent = item.Mesto
+  const removeBtn = newItem.querySelector('.card__trash');
+  removeBtn.addEventListener('click', handleDelete);
+  const likeBtn = newItem.querySelector('.card__button')
+  likeBtn.addEventListener('click', LikeHeart)
+  head.addEventListener('click', imageBig)
+  return newItem;
+}
+function handleAdd() {
+  const inputText = namePicture.value;
+  const inputLink = jobPicture.value;
+  const listItem = getItem({Mesto: inputText, link: inputLink});
+  directorsList.prepend(listItem);
+  namePicture.value = ''
+  jobPicture.value = ''
+}
+function RenderImage(evt){
+  evt.preventDefault()
+  handleAdd()
+  togglePictureOpen()
+}
+form.addEventListener('submit', RenderImage)
+function handleDelete(event) {
+  const targetEl = event.target;
+  const targetItem = targetEl.closest('.card');
+  targetItem.remove();
+}
+let LikeHeart = (item) => { 
+  item.target.classList.toggle('card__button_active')
+}
+function imageBig(item){
+  item.classList.toggle('card__picture_active')
+}
+render();
